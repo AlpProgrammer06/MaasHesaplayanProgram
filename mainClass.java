3502a38d19449ea25074bd0f6c69778f6fee58d2
Aslan BALCI <105630568+AlpProgrammer06@users.noreply.github.com>
2022-08-10 13:42:25 +0300

    Add files via upload

---
+++ mainClass.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee aslan=new Employee("Aslan BALCI", 15000,50,2010);
+            aslan.print();
+        }
+
+
+    }
+
